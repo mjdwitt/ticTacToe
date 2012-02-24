ef57439af67bd981a6d9813e634dff7a4f6f17aa
Michael DeWitt <md@hackfoolery.com>
2012-02-24 11:57:21 -0500

    I really should have made an API for abstracting access to the board.

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -18,3 +18,19 @@ move (Board board) player (x,y)
         (moveRow, tailRows) = splitAt 1 restRows
         (leadCols,restCols) = splitAt x $ head moveRow
         (moveCol, tailCols) = splitAt 1 restCols
+
+
+
+-- | True if the game has been won, i.e. one player has three marks
+-- in a single row, column, or diagonal.
+gameWon :: Board -> Bool
+
+gameWon (Board board) = won board
+  where won board = rowWon board || colWon board || diagWon board
+        rowWon row:rows = snd (foldr homRow (head row, True) row)
+                        || rowWon rows
+          where homRow cell (c,True) | c == cell = (c,True)
+                                     | otherwise = (c,False)
+                homRow _    (c,False)            = (c,False)
+        rowWon    []    = False
+        colWon 
