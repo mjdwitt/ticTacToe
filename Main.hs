e47720bad4644f53ac0f8bdc640c34742489bea6
Michael DeWitt <md@hackfoolery.com>
2012-03-15 15:16:35 -0400

    Created a cli game using the TicTacToe library.

---
+++ Main.hs
@@ -0,0 +1,58 @@
+module Main where
+
+
+
+import TicTacToe
+
+
+
+main :: IO()
+main = do
+       putStrLn "Get ready for some ASCii TicTacToe!"
+       putStrLn ""
+       putStrLn "How many rows and columns should this board have?"
+       size <- getLine
+       play X . makeBoard $ read size
+
+
+
+play :: Player -> Board -> IO()
+play p b = do
+           putStr $ show b
+           case end of
+             NotDone   -> do
+                          putStr (show p ++ "'s turn. ")
+                          turn p b
+             otherwise -> gameOver end
+  where end = winner b
+
+
+
+turn :: Player -> Board -> IO()
+turn p b = do
+           putStrLn "Where would you like to go?"
+           putStr "Select a column: "
+           col <- getLine
+           putStr "Select a row: "
+           row <- getLine
+           case move b p (read col, read row) of
+             Just new -> play (nextPlayer p) new
+             Nothing  -> do
+                         putStrLn "That was an invalid move. Try again."
+                         turn p b
+
+
+
+gameOver :: EndGame -> IO()
+gameOver end = do
+               putStr "Game over. "
+               case end of
+                 Winner a -> putStrLn (show a ++ " won!")
+                 Draw     -> putStrLn "CAT."
+
+
+
+nextPlayer :: Player -> Player
+nextPlayer X = O
+nextPlayer O = X
+nextPlayer _ = error "nextPlayer: only works for X and O"
--- TicTacToe.hs
+++ TicTacToe.hs
@@ -1,36 +1,211 @@
-module TicTacToe where
+module TicTacToe 
+  ( Player(X, O)
+  , Loc
+  , Board()
+  , EndGame(..)
+  , makeBoard
+  , getPlayer
+  , getRow
+  , getCol
+  , getDiag
+  , listRows
+  , listCols
+  , listDiags
+  , unsafeRow
+  , unsafeCol
+  , unsafeDiag
+  , sideLength
+  , move
+  , winner
+  ) where
 
-import Board
+import Data.Map as M (Map, lookup, size, fromList, toList,
+                      filterWithKey, insert)
+import Data.List as L (map, mapAccumL)
 
 
 
--- | Using an existing board, the mark to make, and the coordinates of the
--- Cell in which to place the mark, move produces the Board resulting from
--- placing that mark on the given board.
-move :: Board -> Cell -> Loc -> Board
+data Player = X
+            | O
+            | Empty
+            | Div
+              deriving(Eq)
 
-move (Board board) player (x,y)
-  | head moveCol == Empty
-    = Board $ leadRows ++ [leadCols ++ [player] ++ tailCols] ++ tailRows
-  | otherwise
-    = error "move: location already taken"
-  where (leadRows,restRows) = splitAt y board
-        (moveRow, tailRows) = splitAt 1 restRows
-        (leadCols,restCols) = splitAt x $ head moveRow
-        (moveCol, tailCols) = splitAt 1 restCols
+instance Show Player where
+  show X     = "X"
+  show O     = "O"
+  show Empty = " "
+  show Div   = "-"
 
 
 
--- | True if the game has been won, i.e. one player has three marks
--- in a single row, column, or diagonal.
-gameWon :: Board -> Bool
+type Loc = (Int,Int)
 
-gameWon (Board board) = won board
-  where won board = rowWon board || colWon board || diagWon board
-        rowWon row:rows = snd (foldr homRow (head row, True) row)
-                        || rowWon rows
-          where homRow cell (c,True) | c == cell = (c,True)
-                                     | otherwise = (c,False)
-                homRow _    (c,False)            = (c,False)
-        rowWon    []    = False
-        colWon 
+
+
+data Board = Board (Map Loc Player) 
+             deriving(Eq)
+
+instance Show Board where
+  show board@(Board b) =  "\n"
+                       ++ foldr row "" (L.map build $ range board)
+                       ++ "\n"
+    where build x = unsafeRow board x
+          row cells []   = foldr (cell " | ") "" cells
+          row cells out  = foldr (cell " | ") "" cells
+                        ++ "\n"
+                        ++ foldr (cell "-+-") "" (L.map (\_ -> Div) $
+                                                range board)
+                        ++ "\n"
+                        ++ out
+          cell _   c []  = show c
+          cell div c out = show c ++ div ++ out
+
+
+
+-- | 'makeBoard' @size@ creates a Board filled with empty cells. The Board
+-- will be a square with sides as long as @size@.
+makeBoard :: Int -> Board
+
+makeBoard size = Board . fromList $
+                 zip [ (x,y) | x <- [0..(size - 1)], y <- [0..(size - 1)] ]
+                     (repeat Empty)
+
+
+
+-- | Used to control the ending of a game.
+data EndGame = Winner Player
+             | Draw
+             | NotDone
+               deriving (Show, Eq)
+
+
+
+-- | `winner` @board@ returns @Winner X@ or @Winner O@ if either has three
+-- in a row anywhere on the @board@. If all the spaces are filled, then
+-- @Draw@ is returned. Otherwise, @NotDone@ is returned.
+winner :: Board -> EndGame
+
+winner board | [X,X,X] `elem` rowsColsAndDiags = Winner X
+             | [O,O,O] `elem` rowsColsAndDiags = Winner O
+             | hasEmpties rowsColsAndDiags     = NotDone
+             | otherwise                       = Draw
+  where rowsColsAndDiags = listRows board
+                        ++ listCols board
+                        ++ listDiags board
+        hasEmpties = any (Empty `elem`)
+
+
+
+-- | 'move' @player loc@ inserts the @player@'s mark at @loc@.
+move :: Board -> Player -> Loc -> (Maybe Board)
+
+move board@(Board b) player loc@(c,r)
+  | c < sideLength board && r < sideLength board =
+    Just . Board $ M.insert loc player b
+  | otherwise    = Nothing
+
+
+
+-- | 'getPlayer' @board loc@ returns the value of the cell at @loc@ on
+-- @board@, wrapped in @Just@. If @loc@ is invalid (i.e. not on the
+-- @board@) it returns @Nothing@.
+getPlayer :: Board -> Loc -> Maybe Player
+
+getPlayer (Board b) loc = loc `M.lookup` b
+
+
+
+-- | Some safe versions of the above. Really just wrapper functions.
+
+getRow :: Board -> Int -> Maybe [Player]
+getRow board row | row `outOfBounds` board = Nothing
+                 | otherwise               = Just $ unsafeRow board row
+
+getCol :: Board -> Int -> Maybe [Player]
+getCol board col | col `outOfBounds` board = Nothing
+                 | otherwise               = Just $ unsafeCol board col
+
+getDiag :: Board -> Int -> Maybe [Player]
+getDiag board diag | diag < 0 || diag > 1  = Nothing
+                   | otherwise             = Just $ unsafeDiag board diag
+
+
+
+-- | These accessors use the above to return lists of rows, columns, and
+-- diagonals.
+
+listRows :: Board -> [[Player]]
+listRows board = generalList (unsafeRow board) board
+
+listCols :: Board -> [[Player]]
+listCols board = generalList (unsafeCol board) board
+
+listDiags :: Board -> [[Player]]
+listDiags board = unsafeDiag board 0 : (unsafeDiag board 1 : [])
+
+
+
+-- | The following accessors return lists containing the cells in the
+-- specified row, column, or diagonal. Rows and columns start counting
+-- from zero and there are only two returnable diagonals: zero and one.
+-- The returned list will be the same length as the number provided to
+-- makeBoard.
+
+unsafeRow :: Board -> Int -> [Player]
+unsafeRow (Board b) row = cellsOnly . toList $ filterWithKey matchRow b
+  where matchRow (_,r) _ = row == r
+
+unsafeCol :: Board -> Int -> [Player]
+unsafeCol (Board b) col = cellsOnly . toList $ filterWithKey matchCol b
+  where matchCol (c,_) _ = col == c
+
+unsafeDiag :: Board -> Int -> [Player]
+unsafeDiag bd@(Board b) diag = cellsOnly . toList $
+  filterWithKey matchDiag b
+  where matchDiag (c,r) _ | diag == 0 = c == r
+                          | otherwise = c+r == sideLength bd - 1
+
+
+
+-- | 'sideLength' returns the length of a side of the given @Board@.
+sideLength :: Board -> Int
+
+sideLength (Board b) = floor
+                     . sqrt
+                     $ fromIntegral (size b)
+
+
+
+-- Internal functions
+
+-- | 'cellsOnly' takes a list of (Loc,Player), such as what would be
+-- returned by running toList on a Board, and returns a list containing only
+-- the cells from the list.
+cellsOnly :: [(Loc,Player)] -> [Player]
+
+cellsOnly pairs = L.map (\(_,c) -> c) pairs
+
+
+
+-- | 'range' returns a list of whole numbers from zero to the length of the
+-- given @Board@.
+range board = [0..(sideLength board - 1)]
+
+
+
+-- | 'outOfBounds' @n board@ checks to see if the value @n@ fits within the
+-- dimensions of the @board@. That is, 'outOfBounds' returns @False@ iff
+-- @n < sideLength board@.
+outOfBounds :: Int -> Board -> Bool
+
+outOfBounds n board = n >= sideLength board
+
+
+
+-- | 'generalList' is a generalization used by all the batch accessors. It
+-- uses @get@ to return the 2D list of each group of Players accessible by
+-- @get@.
+generalList :: (Int -> [Player]) -> Board -> [[Player]]
+
+generalList get board = L.map get $ range board
